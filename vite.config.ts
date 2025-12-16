31d7a04dc708cc93f31f83b651d49605e1a5849f
Zanyar HajiEsmailPoor <zanyar.hajiesmailpoor@gmail.com>
2025-12-16 19:45:45 +0000

    setup github actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pneumonia_detection/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
